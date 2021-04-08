5cdd64891b0b8e7d8e12607af972eb68f484574d
Jessi Wu <whale6248@gmail.com>
2021-04-07 22:41:22 -0700

    edit home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './dog_paw.svg';
 import './App.css';
+import SearchBar from './searchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>TheLitterPicker.com</h1>
+        <SearchBar className="SearchBar"/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
