e67079107e6a807a7ba066a211ae6881d35a5264
Jessi Wu <whale6248@gmail.com>
2021-04-19 21:52:46 -0700

    edit home page

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import logo from './dog_paw.svg';
-import './Home.css';
+import AppIcon from './dog_lover.png';
 import SearchBar from './searchBar';
+import { withStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
-function Home() {
-    return (
-        <div className="Home">
-            <h1>TheLitterPicker.com</h1>
-            {/* <img src={logo} className="Home-logo" alt="logo" /> */}
-            <SearchBar className="SearchBar" />
-        </div>
-    );
+const styles = {
+    root: {
+        margin: '0px 0px 0px 0px',
+        textAlign: 'center',
+    },
+    grid_container: {
+        justify: 'center',
+    }
+};
+
+export class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Grid container spacing={3} className={classes.root}>
+                <Grid item xs={12}>
+                    <h1>TheLitterPicker.com</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <img src={AppIcon} alt="dog_lover" width="300" height="300" />
+                </Grid>
+                <Grid item xs={12}>
+                    <SearchBar />
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
 
--- src/searchBar.js
+++ src/searchBar.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import SearchField from 'react-search-field';
 import { useHistory } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import Grid from '@material-ui/core/Grid';
 
-function SearchBar() {
-    let history = useHistory();
 
-    function onChange() {
-        // console.log(this.searchText);    
-    }
-    
-    function onEnter() {
-        history.push("/home");
-    }
-    
-    function onSearchClick() {
-        // let path = this.searchText;
-        let path = '/search';
-        console.log(path);    
-        history.push(path);
+export class SearchBar extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {};
     }
 
-    return (
-        <SearchField
-            placeholder="Enter any dog breeds"
-            onChange={onChange}
-            onEnter={onEnter}
-            onSearchClick={onSearchClick}
-            searchText=""
-            classNames="search field"
-        />);
+    // onChange() {
+    //     // console.log(this.searchText);    
+    // }
+
+    // onEnter() {
+    //     history.push("/home");
+    // }
+
+    // onSearchClick() {
+    //     // let path = this.searchText;
+    //     let path = '/search';
+    //     console.log(path);
+    //     history.push(path);
+    // }
+
+    render() {
+        // const { history } = useHistory();
+        return (
+            <Grid item container xs={12} direction='row' justify='center' alignItems='center'>
+                <Grid item>
+                    <TextField label='Search'></TextField>
+                </Grid>
+                <Grid item>
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
+
+        );
+    }
+
 }
 
 export default SearchBar;
